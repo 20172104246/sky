dc628288671f3083477acf76c7496997b2f788eb
20172104246 <1726857237@qq.com>
2018-05-29 15:34:01 +0800

    哈哈,随意写的程序

--- sky/sky.cpp
+++ sky/sky.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
-
-
+#include<iostream>
+using namespace std;
+void swap(int a, int b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	cout << "In swap:a= " << a << "      b=" << b << endl;
+}
 int main()
 {
-    return 0;
+	{
+		int a = 4;
+		int b = 5;
+		swap(a, b);
+		cout << "In swap:a = " << a << "b = " << b;
+	}
+	return 0;
 }
 
